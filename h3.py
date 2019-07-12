77855905e77e153735935e23e019834801faf5f5
raghavrv555 <51222568+raghavrv555@users.noreply.github.com>
2019-07-12 17:19:26 +0530

    Create h3.py

---
+++ h3.py
@@ -0,0 +1,12 @@
+n=input()
+n= int(input())
+lis = list(map(str,input().split()))
+ans=[]
+for i in range(n):
+    if int(lis[i])==i:
+        ans.append(lis[i])
+if(len(ans)==0):
+    print(-1)
+else:
+    print(" ".join(ans))
+    
