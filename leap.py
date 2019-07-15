b6e30616d1c68e072bf1c2133306c0e4e54a6ebf
raghavrv555 <51222568+raghavrv555@users.noreply.github.com>
2019-07-15 15:30:19 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,13 @@
+x=int(input())
+if(x%4==0):
+    if(x%100==0):
+        if(x%400==0):
+            print("yes")
+        else:
+            print("no")
+    else:
+        print("yes")
+else:
+    print("no")
+    
+    print ()
