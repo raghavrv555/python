ea3f658dca2573df1d536e6462422a0f32a05305
raghavrv555 <51222568+raghavrv555@users.noreply.github.com>
2019-07-15 15:52:15 +0530

    Create pr3.py

---
+++ pr3.py
@@ -0,0 +1,12 @@
+a,b=input().split()
+h=list(map(int,input().split()))
+for p in range(int(b)):
+	u,v=input().split()
+	sum=0
+	for z in range(int(u)-1,int(v)):
+		min=h[int(u)-1]
+		if h[z]<min:
+			min=h[z]	
+	print(min)
+	continue
+  gh
